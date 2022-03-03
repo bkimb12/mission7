9f4476760b012014a07bc9b42cbea3babf915223
bkimb12 <braedonk@byu.edu>
2022-03-02 17:44:22 -0700

    mission 9 changes and commit

--- mission7/Models/Basket.cs
+++ mission7/Models/Basket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace mission7.Models
     {
         public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
 
-        public void AddItem(Book book, int qty, float price)
+        public virtual void AddItem(Book book, int qty, float price)
         {
             BasketLineItem line = Items
                 .Where(b => b.Book.BookId == book.BookId)
@@ -30,6 +31,17 @@ namespace mission7.Models
             }
         }
 
+        public virtual void RemoveItem(Book book)
+        {
+            Items.RemoveAll(x => x.Book.BookId == book.BookId);
+        }
+
+        public virtual void ClearBasket()
+        {
+            Items.Clear();
+        }
+
+
         public double CalculateTotal()
         {
             double sum = Items.Sum(x => x.Quantity * x.Price);
@@ -40,6 +52,7 @@ namespace mission7.Models
 
     public class BasketLineItem 
     {
+        [Key]
         public int LineID { get; set; }
         public Book Book { get; set; }
         public int Quantity { get; set; }
--- mission7/Models/BookstoreContext.cs
+++ mission7/Models/BookstoreContext.cs
@@ -20,6 +20,7 @@ namespace mission7.Models
         }
 
         public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
 
     }
 }

5bb21c5b2cb211c69ffd060c6e9b22696b41afe9
bkimb12 <braedonk@byu.edu>
2022-03-10 16:54:15 -0700

    commit for mission 10

--- mission7/Migrations/20220308234156_AddReceivedField.Designer.cs
+++ mission7/Migrations/20220308234156_AddReceivedField.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mission7.Models;
 
 namespace mission7.Migrations
 {
     [DbContext(typeof(BookstoreContext))]
-    partial class BookstoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220308234156_AddReceivedField")]
+    partial class AddReceivedField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
